cb157320b007cb759cbdb493c555474bd36aed05
Jakob <jakob.parker16@gmail.com>
2025-10-08 16:40:49 +0100

    Update bulb_finder.py

--- bulb_finder.py
+++ bulb_finder.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-PASSWORD = "M3jf7U9A1FjMsPVF"
+PASSWORD = st.secrets["PASSWORD"]
 
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
@@ -17,7 +17,7 @@ if not st.session_state.logged_in:
             st.error("Incorrect password")
 
 if st.session_state.logged_in:
-    st.title("Bulb Install Lookup")
+    st.title("Bulb Installs Lookup App")
 
     @st.cache_data
     def load_csvs():
@@ -27,38 +27,28 @@ if st.session_state.logged_in:
 
     elec_df, gas_df = load_csvs()
     
-    
     dataset_choice = st.radio("Select fuel to search:", ["Electricity", "Gas"])
+    df = elec_df if dataset_choice == "Electricity" else gas_df
 
-    if dataset_choice == "Electricity":
-        df = elec_df
-    else:
-        df = gas_df
+    columns_to_show = ['Device ID', 'Meter Serial Number', 'Install Code', 'Device Model', 'Device Firmware Version']
 
+    search_key = st.text_input(f"Enter Device ID or Meter Serial Number to lookup")
+    results = pd.DataFrame()  # initialize to avoid NameError
 
-    columns_to_show = ['Device ID', 'Meter Serial Number',  'Install Code', 'Device Model', 'Device Firmware Version']
-
-
-    search_key = st.text_input(f"Enter GUID or MSN to lookup")
-
-if search_key:
-    results = df[
-        (df['Device ID'].astype(str) == search_key) |
-        (df['Meter Serial Number'].astype(str) == search_key)
-    ]
-
+    if search_key:
+        results = df[
+            (df['Device ID'].astype(str) == search_key) |
+            (df['Meter Serial Number'].astype(str) == search_key)
+        ]
+    
     if not results.empty:
         results_display = results[columns_to_show]
         
-        st.write(f"Found {len(results_display)} row(s):")
+        st.write(f"Found {len(results)} row(s):")
         st.dataframe(results_display)
         
         csv = results_display.to_csv(index=False).encode('utf-8')
-        st.download_button(
-            label="Download result as CSV",
-            data=csv,
-            file_name=f"{dataset_choice}_lookup_{search_key}.csv",
-            mime='text/csv'
-        )
     else:
-        st.warning("No matching records found.")
+        if search_key:
+            st.warning("No matching records found.")
+
